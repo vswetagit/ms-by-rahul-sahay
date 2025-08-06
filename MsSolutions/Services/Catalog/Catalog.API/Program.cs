ca6313c8b3081c925644478cf784829516d46403
Sweta <sweta_v@hotmail.com>
2025-08-06 07:34:25 +0530

    Swagger metadata added Added entities Added repositories

--- MsSolutions/Services/Catalog/Catalog.API/Program.cs
+++ MsSolutions/Services/Catalog/Catalog.API/Program.cs
@@ -5,7 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "Catalog.API v1",
+        Version = "ver1"
+    });
+});
 
 var app = builder.Build();
 
---
+++ MsSolutions/Services/Catalog/Catalog.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<IEnumerable<Product>> GetProductsByName(string name);
+        Task<IEnumerable<Product>> GetProductsByBrand(string brandId);
+        Task<IEnumerable<Product>> GetProductsByType(string typeId);
+        Task<Product> CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}
