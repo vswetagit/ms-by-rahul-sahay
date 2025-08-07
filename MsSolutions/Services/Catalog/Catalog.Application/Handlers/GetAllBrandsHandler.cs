8913b5b368c19fa078053281d5f8f51c889c4ae9
V Sweta <v.sweta@molgroup.com>
2025-08-07 06:45:53 +0530

    Added handler, mapper, queries and response

---
+++ MsSolutions/Services/Catalog/Catalog.Application/Handlers/GetAllBrandsHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetAllBrandsHandler : IRequestHandler<GetAllBrandsQuery, IList<BrandResponse>>
+    {
+        private readonly IBrandRepository _repository;
+        private readonly IMapper _mapper;
+        public GetAllBrandsHandler(IBrandRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<IList<BrandResponse>> Handle(GetAllBrandsQuery request, CancellationToken cancellationToken)
+        {
+            //var respList = await _repository.GetAllBrands();
+            //var output = _mapper.Map<IList<BrandResponse>>(respList);
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MsSolutions/Services/Catalog/Catalog.Application/Mappers/ProductMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Catalog.Application.Responses;
+using Catalog.Core.Entities;
+
+namespace Catalog.Application.Mappers
+{
+    public class ProductMappingProfile:Profile
+    {
+        public ProductMappingProfile()
+        {
+            CreateMap<ProductBrand, BrandResponse>().ReverseMap();
+            
+        }
+    }
+}
