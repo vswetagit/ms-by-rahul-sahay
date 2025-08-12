92f0efc62259807ed8cfec124c077a9e5b88d434
Sweta <sweta_v@hotmail.com>
2025-08-12 06:17:46 +0530

    Test

---
+++ MsSolutions/Services/Catalog/Catalog.Application/Handlers/GetProductByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Catalog.Application.Mappers;
+using Catalog.Application.Queries;
+using Catalog.Application.Responses;
+using Catalog.Core.Repositories;
+using MediatR;
+
+namespace Catalog.Application.Handlers
+{
+    public class GetProductByIdHandler : IRequestHandler<GetProductByIdQuery,
+        ProductResponse>
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductRepository _repository;
+        public GetProductByIdHandler(IMapper mapper, IProductRepository repository)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+        public async Task<ProductResponse> Handle(GetProductByIdQuery request,
+            CancellationToken cancellationToken)
+        {
+            var items = await _repository.GetProduct(request.Id);
+            var output = ProductMapper.Mapper.Map<ProductResponse>(items);
+            return output;
+        }
+    }
+}
---
+++ MsSolutions/Services/Catalog/Catalog.Application/Queries/GetProductByNameQuery.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Application.Responses;
+using MediatR;
+
+namespace Catalog.Application.Queries
+{
+    public class GetProductByNameQuery : IRequest<IList<ProductResponse>>
+    {
+        public string Name { get; set; }
+    }
+}
