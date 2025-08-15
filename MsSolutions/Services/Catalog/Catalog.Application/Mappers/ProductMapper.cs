3b8ed2ee43ac326e62e8853e8b82cb33dbde6c10
Sweta <sweta_v@hotmail.com>
2025-08-15 15:18:07 +0530

    Added logger factory to mapper

--- MsSolutions/Services/Catalog/Catalog.Application/Mappers/ProductMapper.cs
+++ MsSolutions/Services/Catalog/Catalog.Application/Mappers/ProductMapper.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
 
 namespace Catalog.Application.Mappers
 {
@@ -6,11 +7,17 @@ namespace Catalog.Application.Mappers
     {
         private static readonly Lazy<IMapper> mapper = new Lazy<IMapper>(() =>
         {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddFilter("Microsoft", LogLevel.Warning)
+                       .AddFilter("System", LogLevel.Warning)
+                       .AddFilter("SampleApp.Program", LogLevel.Debug);
+            });
             var config = new MapperConfiguration(c =>
             {
                 c.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
                 c.AddProfile<ProductMappingProfile>();
-            }, null);
+            },loggerFactory);
             var mapper = config.CreateMapper();
             return mapper;
         });
